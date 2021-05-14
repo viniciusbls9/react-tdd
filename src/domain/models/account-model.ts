4ac5f3cff60dbc44e28f312fd71a01fb2ca43229
Vinicius Benedito <beneditoloura@gmail.com>
2021-05-14 07:40:38 -0300

    docs: include comments for documentation

--- src/domain/models/account-model.ts
+++ src/domain/models/account-model.ts
@@ -1,3 +1,7 @@
+/**
+ * Type para mockar o retorno do caso de uso authentication. Esse type é chamado no arquivo usecases/authentication.ts.
+ * A ideia desse arquivo é para se caso um dia a API que estamos usando retornar mais coisas além do accessToken, a gente mexer direto nesse arquivo e não no arquivo de casos de uso. Bom para manter uma organização
+ */
 export type AccountModel = {
   accessToken: string
 }
--- src/domain/usecases/authentication.ts
+++ src/domain/usecases/authentication.ts
@@ -1,5 +1,6 @@
 import { AccountModel } from '@/domain/models/account-model'
 
+// Objeto em forma de Type para armazenar os parametros da interface Authentication
 export type AuthenticationParams = {
   email: string
   password: string
