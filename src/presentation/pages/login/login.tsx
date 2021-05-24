ca4be3206e4a78a9fd2570d147d2438e50caa8a2
Vinicius Benedito <beneditoloura@gmail.com>
2021-05-24 07:47:30 -0300

    refactor: move header and footer to separate file

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import Styles from './login-styles.scss'
 import Spinner from '@/presentation/components/spinner/spinner'
-import Logo from '@/presentation/components/logo/logo'
+import Header from '@/presentation/components/login-header/login-header'
+import Footer from '@/presentation/components/footer/footer'
 
 const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
-      <header className={Styles.header}>
-        <Logo />
-        <h1>4Dev - Enquetes para Programadores</h1>
-      </header>
+      <Header />
       <form className={Styles.form}>
-
         <h2>Login</h2>
         <div className={Styles.inputWrap}>
           <input type="email" name="email" placeholder="Digite seu e-mail" />
@@ -29,7 +26,7 @@ const Login: React.FC = () => {
           <span className={Styles.error}>Erro</span>
         </div>
       </form>
-      <footer className={Styles.footer} />
+      <Footer />
     </div>
   )
 }
