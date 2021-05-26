a578c89edc09b6d432eb853af0d89fd0873d6a03
Vinicius Benedito <beneditoloura@gmail.com>
2021-05-26 08:23:54 -0300

    test: ensure Login start with inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -5,9 +5,19 @@ import Login from './login'
 describe('Login Component', () => {
   test('Should start with initial state', () => {
     const { getByTestId } = render(<Login />)
+    // start error test
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
+    // start submit button test
     const submitButton = getByTestId('submit') as HTMLButtonElement // HTMLButtonElement para poder acessar a propriedade disabled
     expect(submitButton.disabled).toBe(true)
+    // start email status test
+    const emailStatus = getByTestId('email-status')
+    expect(emailStatus.title).toBe('Campo obrigatório')
+    expect(emailStatus.textContent).toBe('🔴')
+    // start password status test
+    const passwordStatus = getByTestId('password-status')
+    expect(passwordStatus.title).toBe('Campo obrigatório')
+    expect(passwordStatus.textContent).toBe('🔴')
   })
 })
