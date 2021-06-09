c562382e825697b90a79d76386ecc24ab15ab372
Vinicius Benedito <beneditoloura@gmail.com>
2021-06-09 07:28:12 -0300

    refactor: move SUT creating to a factory method

--- src/validation/errors/required-field-error.ts
+++ src/validation/errors/required-field-error.ts
@@ -1,5 +1,5 @@
 export class RequiredFieldError extends Error {
-  constructor() {
+  constructor () {
     super('Campo obrigatório')
     this.name = 'RequiredFieldError'
   }
--- src/validation/protocols/field-validation.ts
+++ src/validation/protocols/field-validation.ts
@@ -1,4 +1,4 @@
 export interface FieldValidation {
-    field: string
-    validate (value: string): Error
+  field: string
+  validate: (value: string) => Error
 }
