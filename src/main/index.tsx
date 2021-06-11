eae4d06c45591a250fe97479e858359f7f44f74b
Vinicius Benedito <beneditoloura@gmail.com>
2021-06-11 08:53:35 -0300

    feat: add Login composition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router } from '@/presentation/components'
+import { makeLogin } from './factores/pages/login/login-factory'
 import '@/presentation/styles/global.scss'
 
 ReactDOM.render(
-  <Router />,
+  <Router
+    makeLogin={makeLogin}
+  />,
   document.getElementById('main')
 )
--- src/presentation/components/router/router.tsx
+++ src/presentation/components/router/router.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { Login } from '@/presentation/pages'
 
-const Router: React.FC = () => {
+type Props = {
+  makeLogin: React.FC
+}
+
+const Router: React.FC<Props> = ({ makeLogin }: Props) => {
   return (
     <BrowserRouter>
-    <Switch>
-      <Route path="/login" exact component={Login} />
-    </Switch>
+      <Switch>
+        <Route path="/login" exact component={makeLogin} />
+      </Switch>
     </BrowserRouter>
   )
 }
