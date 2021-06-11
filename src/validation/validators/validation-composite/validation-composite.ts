3b8b2d6a3817d8156af49f3a2ac15593b8824ac3
Vinicius Benedito <beneditoloura@gmail.com>
2021-06-11 08:05:34 -0300

    refactor: change ValidationComposite to use a static build method to create instance

--- src/validation/validators/validation-composite/validation-composite.ts
+++ src/validation/validators/validation-composite/validation-composite.ts
@@ -2,7 +2,12 @@ import { Validation } from '@/presentation/protocols/validation'
 import { FieldValidation } from '@/validation/protocols/field-validation'
 
 export class ValidationComposite implements Validation {
-  constructor (private readonly validator: FieldValidation[]) { }
+  private constructor (private readonly validator: FieldValidation[]) { }
+
+  static build (validator: FieldValidation[]): ValidationComposite {
+    return new ValidationComposite(validator)
+  }
+
   validate (fieldName: string, fieldValue: string): string {
     const validators = this.validator.filter(v => v.field === fieldName)
     for (const validator of validators) {
