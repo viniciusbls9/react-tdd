6fc972d1e38dcf65f5453d4606fa0882b69238d9
Vinicius Benedito <beneditoloura@gmail.com>
2021-06-18 08:47:36 -0300

    refactor: update eslint-standard-with-typescript version

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -9,6 +9,6 @@ export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
   async post (params: HttpPostParams<T>): Promise<HttpResponse<R>> {
     this.url = params.url
     this.body = params.body
-    return await Promise.resolve(this.response)
+    return this.response
   }
 }
