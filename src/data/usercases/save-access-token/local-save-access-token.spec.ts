75a7a723d243e11345031af551f3f17c595acb6f
Vinicius Benedito <beneditoloura@gmail.com>
2021-06-23 08:28:04 -0300

    refactor: rename data mocks

--- src/data/usercases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usercases/save-access-token/local-save-access-token.spec.ts
@@ -1,4 +1,4 @@
-import { SetStorageMock } from '@/data/test/mock-storage'
+import { SetStorageMock } from '@/data/test'
 import { LocalSaveAccessToken } from './local-save-access-token'
 import faker from 'faker'
 
