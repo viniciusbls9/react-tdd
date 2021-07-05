eeda5613226c90932b9ae0509f8e73a1c006ab5b
Vinicius Benedito <beneditoloura@gmail.com>
2021-07-05 07:19:05 -0300

    test: ensure show valid email state if Validation succeeds

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -72,4 +72,10 @@ describe('SignUp Component', () => {
     Helper.populateField(sut, 'name')
     Helper.testStatusForField(sut, 'name')
   })
+
+  test('Should show valid email state if Validation succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'email')
+    Helper.testStatusForField(sut, 'email')
+  })
 })
