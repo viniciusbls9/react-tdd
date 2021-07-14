be2b9604a00dafde17ce7ce921979c1e775126aa
Vinicius Benedito <beneditoloura@gmail.com>
2021-07-14 08:55:20 -0300

    feat: add SignUp Composition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router } from '@/presentation/components'
 import { makeLogin } from './factories/pages/login/login-factory'
+import { makeSignup } from './factories/pages/signup/signup-factory'
 import '@/presentation/styles/global.scss'
 
 ReactDOM.render(
   <Router
     makeLogin={makeLogin}
+    makeSignUp={makeSignup}
   />,
   document.getElementById('main')
 )
