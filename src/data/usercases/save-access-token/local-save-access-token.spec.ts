34bd5913f135ef33d580c7aa2873e5a7a5a789be
Vinicius Benedito <beneditoloura@gmail.com>
2021-07-26 08:35:55 -0300

    feat: ensure LocalSaveAccessToken throws if no accessToken is provided

--- src/data/usercases/save-access-token/local-save-access-token.spec.ts
+++ src/data/usercases/save-access-token/local-save-access-token.spec.ts
@@ -1,4 +1,5 @@
 import { SetStorageMock } from '@/data/test'
+import { UnexpectedError } from '@/domain/errors'
 import { LocalSaveAccessToken } from './local-save-access-token'
 import faker from 'faker'
 
@@ -30,5 +31,11 @@ describe('LocalSaveAccessToken', () => {
     jest.spyOn(setStorageMock, 'set').mockRejectedValueOnce(new Error())
     const promise = sut.save(faker.datatype.uuid())
     await expect(promise).rejects.toThrow(new Error())
+  })
+
+  test('Should throw if accessToken is falsy', async () => {
+    const { sut } = makeSut()
+    const promise = sut.save(undefined)
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
--- src/data/usercases/save-access-token/local-save-access-token.ts
+++ src/data/usercases/save-access-token/local-save-access-token.ts
@@ -1,10 +1,14 @@
 import { SetStorage } from '@/data/protocols/cache/set-storage'
+import { UnexpectedError } from '@/domain/errors'
 import { SaveAccessToken } from '@/domain/usecases/save-access-token'
 
 export class LocalSaveAccessToken implements SaveAccessToken {
   constructor (private readonly setStorage: SetStorage) {}
 
   async save (accessToken: string): Promise<void> {
+    if (!accessToken) {
+      throw new UnexpectedError()
+    }
     await this.setStorage.set('accessToken', accessToken)
   }
 }
