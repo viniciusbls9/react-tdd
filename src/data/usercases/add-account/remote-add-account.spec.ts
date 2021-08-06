c5b2b1eefd6d1d4f9c769b67199628df29530cbc
Vinicius Benedito <beneditoloura@gmail.com>
2021-08-06 08:17:31 -0300

    refactor: remove generic body type of http post client

--- src/data/usercases/add-account/remote-add-account.spec.ts
+++ src/data/usercases/add-account/remote-add-account.spec.ts
@@ -1,7 +1,6 @@
 import { RemoteAddAccount } from './remote-add-account'
 import { HttpPostClientSpy } from '@/data/test'
 import { HttpStatusCode } from '@/data/protocols/http'
-import { AddAccountParams } from '@/domain/usecases'
 import { mockAddAccountParams, mockAccountModel } from '@/domain/test'
 import { EmailInUserError, UnexpectedError } from '@/domain/errors'
 import { AccountModel } from '@/domain/models'
@@ -9,11 +8,11 @@ import faker from 'faker'
 
 type SutTypes = {
   sut: RemoteAddAccount
-  httpPostClientSpy: HttpPostClientSpy<AddAccountParams, AccountModel>
+  httpPostClientSpy: HttpPostClientSpy<AccountModel>
 }
 
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy<AddAccountParams, AccountModel>()
+  const httpPostClientSpy = new HttpPostClientSpy<AccountModel>()
   const sut = new RemoteAddAccount(url, httpPostClientSpy)
   return {
     sut,
--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,8 +1,8 @@
 import { HttpPostClient, HttpPostParams, HttpResponse } from '@/data/protocols/http'
 import axios, { AxiosResponse } from 'axios'
-export class AxiosHttpClient implements HttpPostClient<any, any> {
-  async post (params: HttpPostParams<any>): Promise<HttpResponse<any>> {
-    let httpResponse: AxiosResponse<any>
+export class AxiosHttpClient implements HttpPostClient<any> {
+  async post (params: HttpPostParams): Promise<HttpResponse> {
+    let httpResponse: AxiosResponse
     try {
       httpResponse = await axios.post(params.url, params.body)
     } catch (error) {
