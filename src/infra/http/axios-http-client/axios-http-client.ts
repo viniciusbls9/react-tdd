9e89e0a8926fd8ac9310f08134670f5acb06e2e1
Vinicius Benedito <beneditoloura@gmail.com>
2021-08-11 08:12:13 -0300

    refactor: improve test names

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -2,15 +2,15 @@ import { HttpPostClient, HttpPostParams, HttpResponse } from '@/data/protocols/h
 import axios, { AxiosResponse } from 'axios'
 export class AxiosHttpClient implements HttpPostClient<any> {
   async post (params: HttpPostParams): Promise<HttpResponse> {
-    let httpResponse: AxiosResponse
+    let axiosResponse: AxiosResponse
     try {
-      httpResponse = await axios.post(params.url, params.body)
+      axiosResponse = await axios.post(params.url, params.body)
     } catch (error) {
-      httpResponse = error.response
+      axiosResponse = error.response
     }
     return {
-      statusCode: httpResponse.status,
-      body: httpResponse.data
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data
     }
   }
 }
