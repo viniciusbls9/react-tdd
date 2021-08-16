5eb167b5024788882417a3b5257fd121ede17255
Vinicius Benedito <beneditoloura@gmail.com>
2021-08-16 09:25:36 -0300

    refactor: adapt layout to mobile

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -22,6 +22,30 @@ const SurveyList: React.FC = () => {
             </div>
             <footer>Ver resultado</footer>
           </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon className={Styles.iconWrap} iconName={IconName.thumbDown} />
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>03</span>
+                <span className={Styles.year}>2020</span>
+              </time>
+              <p>Qual é seu framework web favorito?</p>
+            </div>
+            <footer>Ver resultado</footer>
+          </li>
+          <li>
+            <div className={Styles.surveyContent}>
+              <Icon className={Styles.iconWrap} iconName={IconName.thumbDown} />
+              <time>
+                <span className={Styles.day}>22</span>
+                <span className={Styles.month}>03</span>
+                <span className={Styles.year}>2020</span>
+              </time>
+              <p>Qual é seu framework web favorito?</p>
+            </div>
+            <footer>Ver resultado</footer>
+          </li>
         </ul>
       </div>
       <Footer />
