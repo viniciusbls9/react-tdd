4305f416c33502bf9c776c80e06475dd823cd5e8
Vinicius Benedito <beneditoloura@gmail.com>
2021-08-16 08:54:05 -0300

    refactor: move header to its own file

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,20 +1,12 @@
 import React from 'react'
-import { Footer, Logo } from '@/presentation/components'
+import { Footer, Header } from '@/presentation/components'
 
 import Styles from './survey-list-styles.scss'
 
 const SurveyList: React.FC = () => {
   return (
     <div className={Styles.surveyListWrap}>
-      <header className={Styles.headerWrap}>
-        <div className={Styles.headerContent}>
-          <Logo />
-          <div className={Styles.logoutWrap}>
-            <span>Rodrigo</span>
-            <a href="#">sair</a>
-          </div>
-        </div>
-      </header>
+      <Header />
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
         <ul>
