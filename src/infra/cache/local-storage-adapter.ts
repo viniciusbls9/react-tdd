4fd2788a513786ac0ea6f3a66509e32f2a5ac946
Vinicius Benedito <beneditoloura@gmail.com>
2021-08-20 08:17:52 -0300

    refactor: remove await set-storage

--- src/infra/cache/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter.ts
@@ -1,7 +1,7 @@
 import { SetStorage } from '@/data/protocols/cache/set-storage'
 
 export class LocalStorageAdapter implements SetStorage {
-  async set (key: string, value: any): Promise<void> {
+  set (key: string, value: any): void {
     localStorage.setItem(key, value)
   }
 }
