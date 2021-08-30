43052c1f3bb529203aac251638a9d70a18ccdc70
Vinicius Benedito <beneditoloura@gmail.com>
2021-08-30 08:34:44 -0300

    refactor: move makeSut to test inside

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -7,8 +7,8 @@ const makeSut = (): void => {
 }
 
 describe('SurveyList Component', () => {
-  makeSut()
   test('Should present 4 empty items on start', () => {
+    makeSut()
     const surveyList = screen.getByTestId('survey-list')
     expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
   })
