3162abee280a54bec84842c790a677490e0ed2f4
Vinicius Benedito <beneditoloura@gmail.com>
2021-09-22 09:07:46 -0300

    feat: ensure AuthorizeHttpGetClientDecorator not add token if GetStorage is invalid

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -2,10 +2,15 @@ import { GetStorage } from '@/data/protocols/cache'
 import { HttpGetClient, HttpGetParams, HttpResponse } from '@/data/protocols/http'
 
 export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
-  constructor (private readonly getStorage: GetStorage) {}
+  constructor (
+    private readonly getStorage: GetStorage,
+    private readonly httpGetClient: HttpGetClient
+
+  ) {}
 
   async get (params: HttpGetParams): Promise<HttpResponse> {
     this.getStorage.get('account')
+    await this.httpGetClient.get(params)
     return null
   }
 }
