be0f843638c06049e8a11d91e3f308921b941f9f
Vinicius Benedito <beneditoloura@gmail.com>
2021-09-24 07:58:55 -0300

    feat: ensure AuthorizeHttpGetClientDecorator add token to HttpGetClient

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.ts
@@ -9,7 +9,14 @@ export class AuthorizeHttpGetClientDecorator implements HttpGetClient {
   ) {}
 
   async get (params: HttpGetParams): Promise<HttpResponse> {
-    this.getStorage.get('account')
+    const account = this.getStorage.get('account')
+    if (account?.accessToken) {
+      Object.assign(params, {
+        headers: {
+          'x-access-token': account.accessToken
+        }
+      })
+    }
     await this.httpGetClient.get(params)
     return null
   }
