909b2a50901b923b8b0c69b678938233cd6a4532
Vinicius Benedito <beneditoloura@gmail.com>
2021-09-29 08:52:02 -0300

    feat: ensure RemoteLoadSurveyList map remote data to domain data

---
+++ src/data/test/mock-remote-survey-list.ts
@@ -0,0 +1,15 @@
+import { RemoteLoadSurveyList } from '@/data/usercases'
+import faker from 'faker'
+
+export const mockRemoteSurveyModel = (): RemoteLoadSurveyList.Model => ({
+  id: faker.datatype.uuid(),
+  question: faker.random.words(10),
+  didAnswer: faker.datatype.boolean(),
+  date: faker.date.recent().toISOString()
+})
+
+export const mockRemoteSurveyListModel = (): RemoteLoadSurveyList.Model[] => ([
+  mockRemoteSurveyModel(),
+  mockRemoteSurveyModel(),
+  mockRemoteSurveyModel()
+])
