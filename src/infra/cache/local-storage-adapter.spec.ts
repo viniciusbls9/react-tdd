d22a4e4e3bc90cd9e74e95b024c883279363aa16
Vinicius Benedito <beneditoloura@gmail.com>
2021-10-01 08:30:22 -0300

    test: ensure LocalStorageAdapter calls removeItem if value is null

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -17,13 +17,18 @@ describe('LocalStorageAdapter', () => {
     expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value))
   })
 
-  test('Should call localStorage.getItem with correct value', async () => {
+  test('Should call localStorage.setItem with correct values', async () => {
     const sut = makeSut()
     const key = faker.database.column()
     const value = faker.random.objectElement<{}>()
-    const getItemSpy = jest.spyOn(localStorage, 'getItem').mockReturnValueOnce(JSON.stringify(value))
-    const obj = sut.get(key)
-    expect(obj).toEqual(value)
-    expect(getItemSpy).toHaveBeenCalledWith(key)
+    sut.set(key, value)
+    expect(localStorage.setItem).toHaveBeenCalledWith(key, JSON.stringify(value))
+  })
+
+  test('Should call localStorage.removeItem if value is null', async () => {
+    const sut = makeSut()
+    const key = faker.database.column()
+    sut.set(key, undefined)
+    expect(localStorage.removeItem).toHaveBeenCalledWith(key)
   })
 })
