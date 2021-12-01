b32b311039e7b618e1702ffc4c37659eb98eeb36
Vinicius Benedito <beneditoloura@gmail.com>
2021-12-01 07:16:44 -0300

    refactor: change SurveyResult to return isCurrentAccountAnswer

--- src/data/test/mock-remote-survey-result.ts
+++ src/data/test/mock-remote-survey-result.ts
@@ -8,11 +8,13 @@ export const mockRemoteSurveyResultModel = (): RemoteLoadSurveyResult.Model => (
     image: faker.internet.url(),
     answer: faker.random.word(),
     count: faker.datatype.number(),
-    percent: faker.datatype.number(100)
+    percent: faker.datatype.number(100),
+    isCurrentAccountAnswer: faker.datatype.boolean()
   },
   {
     answer: faker.random.word(),
     count: faker.datatype.number(),
-    percent: faker.datatype.number(100)
+    percent: faker.datatype.number(100),
+    isCurrentAccountAnswer: faker.datatype.boolean()
   }]
 })
