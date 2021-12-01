428015af60fb93cdafeca7c3db413a5f5b2e12c4
Vinicius Benedito <beneditoloura@gmail.com>
2021-12-01 07:55:06 -0300

    feat: add SurveyResult layout

---
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -0,0 +1,43 @@
+import FlipMove from 'react-flip-move'
+
+import { Footer, Header, Spinner } from '@/presentation/components'
+import React from 'react'
+import Styles from './survey-result-styles.scss'
+
+const SurveyResult: React.FC = () => {
+  return (
+    <div className={Styles.surveyResultWrap}>
+      <Header />
+      <div className={Styles.contentWrap}>
+        <h2>Qual é seu framework web favorito?</h2>
+        <FlipMove className={Styles.answersList}>
+          <li>
+            <img src="https://react-tdd-clean.herokuapp.com/assets/favicon-48x48.png" alt="Imagem da enquente" />
+            <span className={Styles.answer}>ReactJS</span>
+            <span className={Styles.percent}>50%</span>
+          </li>
+          <li className={Styles.active}>
+            <img src="https://react-tdd-clean.herokuapp.com/assets/favicon-48x48.png" alt="Imagem da enquente" />
+            <span className={Styles.answer}>ReactJS</span>
+            <span className={Styles.percent}>50%</span>
+          </li>
+          <li>
+            <img src="https://react-tdd-clean.herokuapp.com/assets/favicon-48x48.png" alt="Imagem da enquente" />
+            <span className={Styles.answer}>ReactJS</span>
+            <span className={Styles.percent}>50%</span>
+          </li>
+        </FlipMove>
+        <button>Voltar</button>
+        <div className={Styles.loadingWrap}>
+          <div className={Styles.loading}>
+            <span>Aguarde...</span>
+            <Spinner isNegative />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
+}
+
+export default SurveyResult
