ba3f7210f05f7b01b8b5e110c59e1a8445a9606c
Vinicius Benedito <beneditoloura@gmail.com>
2021-12-13 07:41:16 -0300

    test: ensure surveyResult calls LoadSurveyResult

---
+++ src/domain/test/mock-survey-result.ts
@@ -0,0 +1,29 @@
+import { LoadSurveyResult } from '@/domain/usecases'
+import faker from 'faker'
+
+export const mockSurveyResultModel = (): LoadSurveyResult.Model => ({
+  question: faker.random.words(10),
+  date: faker.date.recent(),
+  answers: [{
+    image: faker.internet.url(),
+    answer: faker.random.word(),
+    count: faker.datatype.number(),
+    percent: faker.datatype.number(100),
+    isCurrentAccountAnswer: faker.datatype.boolean()
+  }, {
+    answer: faker.random.word(),
+    count: faker.datatype.number(),
+    percent: faker.datatype.number(100),
+    isCurrentAccountAnswer: faker.datatype.boolean()
+  }]
+})
+
+export class LoadSurveyResultSpy implements LoadSurveyResult {
+  callsCount = 0
+  surveyResult = mockSurveyResultModel()
+
+  async load (): Promise<LoadSurveyResult.Model> {
+    this.callsCount++
+    return this.surveyResult
+  }
+}
