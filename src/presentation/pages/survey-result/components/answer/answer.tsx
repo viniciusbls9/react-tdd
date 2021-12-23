3a89e0cff832f64d60a0550f3d6121f680058c6e
Vinicius Benedito <beneditoloura@gmail.com>
2021-12-23 13:00:01 -0300

    refactor: move SurveyResultAnswer to its own component

---
+++ src/presentation/pages/survey-result/components/answer/answer.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Styles from './answer-styles.scss'
+
+type Props = {
+  answer: {
+    image?: string
+    answer: string
+    count: number
+    percent: number
+    isCurrentAccountAnswer: boolean
+  }
+}
+
+const Answer: React.FC<Props> = ({ answer }: Props) => {
+  const activeClassName = answer.isCurrentAccountAnswer ? Styles.active : ''
+  return (
+    <li data-testid="answer-wrap" className={[Styles.answerWrap, activeClassName].join(' ')}>
+      {answer.image && (
+        <img
+          data-testid="images"
+          src={answer.image}
+          alt={answer.answer}
+        />
+      )}
+      <span data-testid="answer" className={Styles.answer}>
+        {answer.answer}
+      </span>
+      <span data-testid="percent" className={Styles.percent}>
+        {answer.percent}%
+      </span>
+    </li>
+  )
+}
+
+export default Answer
--- src/presentation/pages/survey-result/components/result/result.tsx
+++ src/presentation/pages/survey-result/components/result/result.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { useHistory } from 'react-router-dom'
-import { Calendar } from '@/presentation/components'
 import FlipMove from 'react-flip-move'
 import Styles from './result-styles.scss'
+import { Calendar } from '@/presentation/components'
+import { SurveyResultAnswer } from '@/presentation/pages/survey-result/components'
 import { LoadSurveyResult } from '@/domain/usecases'
 
 type Props = {
@@ -22,27 +23,11 @@ const Result: React.FC<Props> = ({ surveyResult }: Props) => {
         <h2 data-testid="question">{surveyResult.question}</h2>
       </hgroup>
       <FlipMove data-testid="answers" className={Styles.answersList}>
-        {surveyResult.answers.map((answer) => (
-          <li
-            data-testid="answer-wrap"
-            key={answer.answer}
-            className={answer.isCurrentAccountAnswer ? Styles.active : ''}
-          >
-            {answer.image && (
-              <img
-                data-testid="images"
-                src={answer.image}
-                alt={answer.answer}
-              />
-            )}
-            <span data-testid="answer" className={Styles.answer}>
-              {answer.answer}
-            </span>
-            <span data-testid="percent" className={Styles.percent}>
-              {answer.percent}%
-            </span>
-          </li>
-        ))}
+        {surveyResult.answers.map((answer) =>
+          <div key={answer.answer}>
+            <SurveyResultAnswer answer={answer} />
+          </div>
+        )}
       </FlipMove>
       <button className={Styles.button} data-testid="back-button" onClick={goBack}>Voltar</button>
     </>
