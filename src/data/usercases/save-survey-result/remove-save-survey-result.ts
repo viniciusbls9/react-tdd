ca6cb29e2f7d0caf20b33bbcfb8b6a847ff6b328
Vinicius Benedito <beneditoloura@gmail.com>
2022-01-05 08:10:00 -0300

    feat: ensure RemoteSaveSurveyResult returns SurveyResult on 2004

--- src/data/usercases/save-survey-result/remove-save-survey-result.ts
+++ src/data/usercases/save-survey-result/remove-save-survey-result.ts
@@ -11,8 +11,9 @@ export class RemoteSaveSurveyResult implements SaveSurveyResult {
 
   async save (params: SaveSurveyResult.Params): Promise<SaveSurveyResult.Model> {
     const httpResponse = await this.httpClient.request({ url: this.url, method: 'put', body: params })
+    const remoteSurveyResult = httpResponse.body
     switch (httpResponse.statusCode) {
-      case HttpStatusCode.ok: return null
+      case HttpStatusCode.ok: return Object.assign({}, remoteSurveyResult, { date: new Date(remoteSurveyResult.date) })
       case HttpStatusCode.forbidden: throw new AccessDeniedError()
       default: throw new UnexpectedError()
     }
