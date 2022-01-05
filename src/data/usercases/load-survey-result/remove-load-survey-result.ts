bffe38c9ec6bf859cf86908dc55a633f34020d18
Vinicius Benedito <beneditoloura@gmail.com>
2022-01-05 07:49:56 -0300

    feat: ensure RemoteSaveSurveyResult calls HttpClient with correct values

--- src/data/usercases/load-survey-result/remove-load-survey-result.ts
+++ src/data/usercases/load-survey-result/remove-load-survey-result.ts
@@ -1,3 +1,4 @@
+import { RemoteSurveyResultModel } from '@/data/models'
 import { HttpClient, HttpStatusCode } from '@/data/protocols/http'
 import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 import { LoadSurveyResult } from '@/domain/usecases'
@@ -20,15 +21,5 @@ export class RemoteLoadSurveyResult implements LoadSurveyResult {
   }
 }
 export namespace RemoteLoadSurveyResult {
-  export type Model = {
-    question: string
-    date: string
-    answers: Array<{
-      image?: string
-      answer: string
-      count: number
-      percent: number
-      isCurrentAccountAnswer: boolean
-    }>
-  }
+  export type Model = RemoteSurveyResultModel
 }
---
+++ src/data/usercases/save-survey-result/remove-save-survey-result.ts
@@ -0,0 +1,18 @@
+import { RemoteSurveyResultModel } from '@/data/models'
+import { HttpClient } from '@/data/protocols/http'
+import { SaveSurveyResult } from '@/domain/usecases'
+
+export class RemoteSaveSurveyResult implements SaveSurveyResult {
+  constructor (
+    private readonly url: string,
+    private readonly httpClient: HttpClient<RemoteSaveSurveyResult.Model>
+  ) {}
+
+  async save (params: SaveSurveyResult.Params): Promise<SaveSurveyResult.Model> {
+    await this.httpClient.request({ url: this.url, method: 'put' })
+    return null
+  }
+}
+export namespace RemoteSaveSurveyResult {
+  export type Model = RemoteSurveyResultModel
+}
